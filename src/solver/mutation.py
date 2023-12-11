2b1fbb60977a0a1a0f9d81976455e986ce6f3682
VChvojka <chvojvoj@fit.cvut.cz>
2023-12-11 23:26:08 +0100

    define random flip

--- src/solver/mutation.py
+++ src/solver/mutation.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+from random import randrange
 from typing import Callable
 
 from weighted_formula.configuration import Configuration
@@ -5,3 +7,14 @@ from weighted_formula.weighted_formula import WeightedCnf
 
 
 mutation_strategy_t = Callable[[WeightedCnf, Configuration], Configuration]
+
+
+def flip_random_variable(unused: WeightedCnf,
+                         config: Configuration) -> Configuration:
+    _ = unused
+
+    random_variable = randrange(config._variable_cnt)
+    mutant = deepcopy(config)
+    mutant.flip_variable(random_variable)
+
+    return mutant
