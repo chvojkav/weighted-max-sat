0075f72a6e4fb0e4730e01854eb22875690dfb6f
VChvojka <chvojvoj@fit.cvut.cz>
2023-12-30 14:16:09 +0100

    Add parameters for running from CLI.

--- main.py
+++ main.py
@@ -11,16 +11,30 @@ from solver.mwcnf_individual import MwcnfIndividual
 def main():
     parser = ArgumentParser()
     parser.add_argument("--formula_file")
+    parser.add_argument("--population_size",
+                        default=200)
+    parser.add_argument("--generation_cnt",
+                        default=1000)
+    parser.add_argument("--tournament_size",
+                        default=1.75)
+    parser.add_argument("--crossover_probability",
+                        default=0.9)
+    parser.add_argument("--mutation_rate",
+                        default=0.03)
+    parser.add_argument("--elitism",
+                        default=1)
     args = parser.parse_args(argv[1:])
 
     formula_path = Path(args.formula_file)
     formula = parse_mwcnf(formula_path)
     solution = genetic_algorithm(MwcnfGenerator(formula),
-                                 population_size=200,
-                                 number_of_generations=1000,
-                                 debug_stream=stderr,
-                                 selection=TournamentSelection(3),
-                                 mutation_rate=0.01)
+                                 population_size=args.population_size,
+                                 number_of_generations=args.generation_cnt,
+                                 selection=TournamentSelection(args.tournament_size),
+                                 crossover_probability=args.crossover_probability,
+                                 mutation_rate=args.mutation_rate,
+                                 elitism=args.elitism,
+                                 debug_stream=stderr)
 
     if isinstance(solution, MwcnfIndividual):
         print(f"Number satisfied: {satisfied_clause_count(formula, solution.config)}")
