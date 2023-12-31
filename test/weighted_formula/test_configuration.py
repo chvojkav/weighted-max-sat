8239a29561bb141abb93f1eb3595f9baa2380dff
VChvojka <chvojvoj@fit.cvut.cz>
2023-12-31 14:04:57 +0100

    Add tests for Configuration.

--- test/weighted_formula/test_configuration.py
+++ test/weighted_formula/test_configuration.py
@@ -11,3 +11,53 @@ def test_from_config_returns_deep_copy():
     assert a.evaluate_variable(1) == b.evaluate_variable(1)
     b.flip_variable(1)
     assert a.evaluate_variable(1) != b.evaluate_variable(1)
+
+def test_sat_literals_counts():
+    a = Configuration(WeightedCnf([(1,2,3), (-1, -2, -3)], (5, 7, 8)))
+    a.set_random()
+    assert a.counts_of_satisfied_literals[0] == (
+        a.evaluate_variable(1) + a.evaluate_variable(2) + a.evaluate_variable(3)
+    )
+    a.set_variable(1, True)
+    a.set_variable(2, True)
+    a.set_variable(3, True)
+    assert a.counts_of_satisfied_literals[0] == 3
+    assert a.counts_of_satisfied_literals[1] == 0
+    a.set_variable(1, False)
+    assert a.counts_of_satisfied_literals[0] == 2
+    assert a.counts_of_satisfied_literals[1] == 1
+    a.set_variable(2, False)
+    assert a.counts_of_satisfied_literals[0] == 1
+    assert a.counts_of_satisfied_literals[1] == 2
+    a.set_variable(3, False)
+    assert a.counts_of_satisfied_literals[0] == 0
+    assert a.counts_of_satisfied_literals[1] == 3
+    a.flip_variable(1)
+    assert a.counts_of_satisfied_literals[0] == 1
+    assert a.counts_of_satisfied_literals[1] == 2
+    a.flip_variable(2)
+    assert a.counts_of_satisfied_literals[0] == 2
+    assert a.counts_of_satisfied_literals[1] == 1
+    a.flip_variable(3)
+    assert a.counts_of_satisfied_literals[0] == 3
+    assert a.counts_of_satisfied_literals[1] == 0
+
+def test_weights():
+    a = Configuration(WeightedCnf([(1,2,3), (-1, -2, -3)], (5, 7, 8)))
+    a.set_random()
+    a.set_variable(1, True)
+    a.set_variable(2, True)
+    a.set_variable(3, True)
+    assert a.sat_weights_sum == 20
+    a.set_variable(1, False)
+    assert a.sat_weights_sum == 15
+    a.set_variable(2, False)
+    assert a.sat_weights_sum == 8
+    a.set_variable(3, False)
+    assert a.sat_weights_sum == 0
+    a.flip_variable(1)
+    assert a.sat_weights_sum == 5
+    a.flip_variable(2)
+    assert a.sat_weights_sum == 12
+    a.flip_variable(3)
+    assert a.sat_weights_sum == 20
