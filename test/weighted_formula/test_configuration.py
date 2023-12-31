7ee2fc74b60e23ad28271adc4452322a8c9e53ec
VChvojka <chvojvoj@fit.cvut.cz>
2023-12-31 11:16:00 +0100

    Fix test.

--- test/weighted_formula/test_configuration.py
+++ test/weighted_formula/test_configuration.py
@@ -1,18 +1,12 @@
 
 from weighted_formula.configuration import Configuration
-
-class FormulaStub:
-    def __init__(self):
-        self.weights_sum = 0
-        self.cnf = None
-        self.clause_cnt = 2
-        self.variable_cnt = 2
+from weighted_formula.weighted_formula import WeightedCnf
 
 
 def test_from_config_returns_deep_copy():
-    a = Configuration(FormulaStub())
+    a = Configuration(WeightedCnf([(1,2,3)], (5, 7, 8)))
     a.set_random()
-    b = Configuration(FormulaStub())
+    b = Configuration(WeightedCnf([(1,2,3)], (5, 7, 8)))
     b.from_config(a)
     assert a.evaluate_variable(1) == b.evaluate_variable(1)
     b.flip_variable(1)
