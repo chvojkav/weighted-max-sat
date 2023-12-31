e5d66cf343440b0e380661c513ab8945e8cd480d
VChvojka <chvojvoj@fit.cvut.cz>
2023-12-31 11:03:27 +0100

    Add helper variable -> for each variable list of its clauses.

--- src/weighted_formula/weighted_formula.py
+++ src/weighted_formula/weighted_formula.py
@@ -15,6 +15,23 @@ class WeightedCnf:
         i = -1
         for i, clause in enumerate(cnf):
             self.cnf += ((clause, i),)
-        
+
         self.clause_cnt = i + 1
         self.variable_cnt = len(weights)
+        self.clauses_per_var: dict[int, Iterable[indexed_clause3_t]] = {}
+
+        for i in range(1, self.variable_cnt + 1):
+            tmp = self._find_clauses_for_var(cnf, i)
+            self.clauses_per_var[i] = tmp
+            self.clauses_per_var[-i] = tmp
+
+    @staticmethod
+    def _find_clauses_for_var(cnf: Iterable[clause3_t], variable_no: int) -> tuple[indexed_clause3_t, ...]:
+        clauses: tuple[indexed_clause3_t, ...] = tuple()
+        for i, clause in enumerate(cnf):
+            for variable in clause:
+                if variable_no == abs(variable):
+                    clauses += ((clause, i),)
+                    break
+
+        return clauses
